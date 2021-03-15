6753020b80443e311bc2675e30c13ac75655573a
apibrac <alexis.pibrac@gmail.com>
2021-03-15 19:19:15 +0100

    Probot logs in one line

--- firebase/functions/src/githubApp/helpers/webhook.ts
+++ firebase/functions/src/githubApp/helpers/webhook.ts
@@ -1,9 +1,15 @@
 import { Server, Probot } from "probot";
 import { probotOptions } from "../config";
 import * as functions from "firebase-functions";
+import { getLog } from "probot/lib/helpers/get-log";
 
 export const serverlessProbot = (fn: (app: Probot) => void) => {
-  const server = new Server({ Probot: Probot.defaults(probotOptions) });
+  const log = getLog({ logLevelInString: true });
+
+  const server = new Server({
+    Probot: Probot.defaults({ ...probotOptions, log }),
+  });
+
   server
     .load(fn)
     .catch((error) => console.error(`Error loading probot app ${error}`));
